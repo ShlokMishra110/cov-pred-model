b45c6026944da8b6893e1e9b44b1879e1bb5a410
Shlok Mishra <132663149+ShlokMishra110@users.noreply.github.com>
2025-07-05 20:11:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You can also use your frontend URL only for more security
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
